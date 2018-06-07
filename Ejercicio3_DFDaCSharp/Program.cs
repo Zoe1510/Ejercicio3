f0bef3dc4cd3e5a02fc1cf5cc8a1f179eaa9762a
Zoe1510 <patriciabermudez465@gmail.com>
2018-06-07 10:50:39 -0400

    first commit

---
+++ Ejercicio3_DFDaCSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3_DFDaCSharp
+{
+    class Program
+    {
+        /*3.	Realizar un C# con un menú utilizando la herramienta switch case.
+         * Dentro del menú debe contener la opción para realizar una multiplicación, 
+         * otra para realizar una multiplicación rusa y por ultimo para salir.
+
+        Lo que debe realizar cada uno. 
+        * Multiplicación: (mirar dfd.)
+        * Multiplicación rusa: (Capturar 2 números y multiplique por el método de la multiplicación rusa, La multiplicación entre ellos positivo con positivo y positivo con negativo si se puede realizar. Pero, negativo con negativo y negativo con positivo no.)
+        
+       Solo se puede utilizar los comandos básicos y las decisiones (if, if else, if muchos else if), for, White y do White.
+ */
+        static void Main(string[] args)
+        {
+        }
+    }
+}
